a6f627729901bb0b7f8a8044d211fd060ea67f4f
arshiajr-coder <75935928+arshiajr-coder@users.noreply.github.com>
2023-09-20 05:37:52 +0330

    commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+function inputChange(){
+    let messageBoxValue = event.target.value
+    if (messageBoxValue.length == 0) {
+        console.log(messageBoxValue);
+        document.querySelector(".send-icon").classList.add("hidden")
+        document.querySelector(".link-voice").classList.remove("hidden")
+    } else{
+        document.querySelector(".send-icon").classList.remove("hidden")
+        document.querySelector(".link-voice").classList.add("hidden")
+    }
+}
